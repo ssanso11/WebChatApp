3e6c4948f8ec0d6f1e34d7018b962e3f0d77ce40
Sebastian Sanso <sebastian@cacao.local>
2020-04-05 22:27:44 -0700

    idk what happened w App.js

--- musicchatfront/src/App.js
+++ musicchatfront/src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import LoggedInScreen from './HomeScreens/LoggedInScreen';
 import LoggedOutScreen from './HomeScreens/LoggedOutScreen'
 import { connect } from 'react-redux';
 import {logoutUser} from './actions/logoutAction'
 
-=======
-import LoginScreen from './LoginScreens/Login.js';
-import InfoScreen from './HomeScreens/InfoScreen.js';
-import PrimaryDashboardScreen from './DashboardScreens/PrimaryDashboardScreen.js'
-import RegisterScreen from './LoginScreens/Register.js'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import LessonsScreen from './VideoChatScreens/LessonsScreen';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import {persistor, store} from './configStore';
-import { 
-  Navbar, 
-  Nav, 
-  NavDropdown, 
-} from "react-bootstrap";
->>>>>>> cb12fdff6ff76b4cb8e5f15b88163a6584e282e1
 
 //root class, all other classes are connected throught this class with react-router
 class App extends React.Component {
@@ -39,7 +17,7 @@ class App extends React.Component {
   }
   currentUser = () => {
     //change this soon
-    if (this.props.user == "not authenticated"){
+    if (this.props.user === "not authenticated"){
       console.log("ERROR, not authenticated");
     }
     else {
@@ -48,50 +26,18 @@ class App extends React.Component {
   };
 
   render() {
-    if(this.props.user != "not authenticated") {
+    if(this.props.user !== "not authenticated") {
       
       return (
         <LoggedInScreen />
       );
 
-<<<<<<< HEAD
     }
     else {
       return (
         <LoggedOutScreen />
       );
     }
-=======
-              {/*
-                A <Switch> looks through all its children <Route>
-                elements and renders the first one whose path
-                matches the current URL. Use a <Switch> any time
-                you have multiple routes, but you want only one
-                of them to render at a time
-              */}
-              <Switch>
-                <Route exact path="/">
-                  <InfoScreen />
-                </Route>
-                <Route exact path="/home">
-                  <InfoScreen />
-                </Route>
-                <Route exact path="/login">
-                  <LoginScreen />
-                </Route>
-                <Route exact path="/register">
-                  <RegisterScreen />
-                </Route>
-                <Route exact path="/lessons">
-                  <PrimaryDashboardScreen />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-        </PersistGate>
-      </Provider>
-    );
->>>>>>> cb12fdff6ff76b4cb8e5f15b88163a6584e282e1
   }
 }
 
