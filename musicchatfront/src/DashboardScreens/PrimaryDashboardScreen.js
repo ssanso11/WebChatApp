fa21be42c042bd4385f9a91dd6a52305a954ed78
macklinrw <macklingaming@gmail.com>
2020-04-06 16:11:44 -0700

    Display temp dash

--- musicchatfront/src/DashboardScreens/PrimaryDashboardScreen.js
+++ musicchatfront/src/DashboardScreens/PrimaryDashboardScreen.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import '../styles/PrimaryDashboard.css'
 
-export default class PrimaryDashboard extends React.Component {
+export class PrimaryDashboardScreen extends Component {
     constructor(props) {
         super(props);
-    }
+      }
+
     render() {
-        return(
-            <div className="container-dash">
-                <h1 className="info-title">Dashboard</h1>
+        return (
+            <div>
+                <h1>Dashboard</h1>
+                <h2>Welcome {this.props.user.auth.username}!</h2>
+                <ul>{this.props.user.auth.email}</ul>
+                <ul>{this.props.user.auth.userId}</ul>
             </div>
-        );
-    }
-}
+        )
+    };
+};
+
+const mapStateToProps = (state) => {
+    const { user } = state;
+    return { user };
+};
+
+const mapDispatchToProps = {
+    
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PrimaryDashboardScreen);
